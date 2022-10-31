30cec80b51a321ba8db52e5b96bc91dcefb514ed
sharpman13 <sharpman2001@gmail.com>
2022-10-31 18:50:54 +0100

    feat: complete task2_write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,54 @@
+const process = require('process');
+const { stdin, stdout } = require('process');
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+const p = path.join( __dirname, 'text.txt');
+
+fs.access(p, (err) => { // проверка есть ли файл?
+  if (err) {
+    writeText(`New file will be created. Please write text below: \n`)
+  } else {
+    writeText(`File is already exist. Please write text below: \n`)
+  }
+});
+
+function checkExit(text) { // проверка вводит ли пользователь exit?
+  if (text.indexOf('exit') !== -1) {
+    console.log('See you later!');
+    process.exit();
+}
+}
+
+
+// function write(caption) {
+
+//   fs.createWriteStream(p);
+//   readline.question(caption, (text) => {
+//     checkExit(text);
+
+//     fs.appendFile(p, text, function (err) {
+//       if (err) throw err;
+//       console.log('Saved!');
+//     });
+//     })
+// }
+
+  process.on('beforeExit', (code) => {
+    console.log('See you later!');
+  });
+
+  function writeText (caption) {
+    stdout.write(caption);
+    readline.on('line', (text) => {
+      checkExit(text);
+      fs.appendFile(p, text + '\n', (err) => {
+        if (err) return;
+        // console.log('Saved!');
+      });
+    });
+  }
+
