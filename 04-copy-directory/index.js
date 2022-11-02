cdca196a026c542ff7cffe94bba7ece7305c2f47
sharpman13 <sharpman2001@gmail.com>
2022-11-02 13:51:06 +0100

    feat: complete task4_copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,31 @@
+const fs = require('fs');
+const fsProm = require('fs/promises');
+const path = require('path');
+const paint = require('chalk');
 
+const wayNew = path.join(__dirname + '/files-copy');
+const wayOld = path.join(__dirname + '/files');
+
+
+fs.access(wayNew, async (err) => { // is folder exist?
+  if (err && err.code === 'ENOENT') {
+    await fsProm.mkdir(wayNew, {recursive: true});
+    copyDir()
+  } else {
+      await deleteDir();
+      copyDir();
+  }
+})
+
+async function copyDir() {
+  const files = await fsProm.readdir(wayOld);
+  for (let item of files) {
+    await fsProm.copyFile(wayOld + '/' + item, wayNew + '/' + item)
+  }
+  console.log(`${paint.green(files.length)} file(s) copied succesfully`)
+}
+
+async function deleteDir() {
+  await fsProm.rm(wayNew, { recursive: true, force: true });
+  fsProm.mkdir(wayNew, {recursive: true});
+}
